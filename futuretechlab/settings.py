ddc5ed13f600609991a186ca1a08dffe5c12215f
diegaum87 <87.c.die@gmail.com>
2023-11-02 10:56:48 -0300

    first commit

--- futuretechlab/settings.py
+++ futuretechlab/settings.py
@@ -94,6 +94,7 @@ if not DEBUG:
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
